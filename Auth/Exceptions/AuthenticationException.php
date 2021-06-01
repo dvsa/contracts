526c022a54aad0f8fc5ecad52707a9e57f1b649e
Joshua License <joshua.license@dvsa.gov.uk>
2021-06-01 15:51:58 +0100

    Add base exception class that all exceptions will extend

---
+++ Auth/Exceptions/AuthenticationException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Dvsa\Contracts\Auth\Exceptions;
+
+/**
+ * The base exception for all exceptions in the Auth contracts.
+ */
+interface AuthenticationException extends \Throwable
+{
+}
