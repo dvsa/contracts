b196fc09136952286d733fc441f54dd2f7d6765d
Joshua License <joshua.license@dvsa.gov.uk>
2021-06-02 11:37:33 +0100

    Refactor the exception to a class instead of an interface

---
+++ Auth/ClientException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Dvsa\Contracts\Auth;
+
+/**
+ * This exception will be thrown when a client library throws an exception.
+ * This allows the consumers to catch this and then appropriately handle the
+ * implementation-specific exception using the `getPrevious()` method.
+ */
+class ClientException extends \Exception
+{
+}
